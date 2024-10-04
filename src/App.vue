dd072b4f0174570b8c3576372dcce8fa872d473a
Muhammad <akun.asifurrohman@gmail.com>
2024-10-04 17:12:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+
+
+</script>
+
+<template>
+  <Navbar />
+  <Hero title="test title" subtitle="test subtitle"/>
+  <HomeCards />
+  <h1>vue jobs</h1>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+        <slot></slot>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
